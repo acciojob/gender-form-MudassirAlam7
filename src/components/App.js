1370b1d7cb03a5e746b24ef87f30204d901194cd
mudassiralam7 <mudassiralam7@gmail.com>
2025-04-25 09:26:04 +0530

    First Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,65 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[gender, setGender] = useState("")
+  const[size, setSize] = useState('')
+
+
+  function handleGenderChange(e){
+    setGender(e.target.value)
+    setSize('')
+  }
+  function handleSizeChange(e){
+    setSize(e.target.value)
+
+  }
+  console.log(gender);
+  console.log(size);
+  
+  
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h2>Select Your Gender</h2>
+      <label>
+        <input type="radio" name="gender" value= "male" checked= {gender=="male"}  onChange={handleGenderChange} />
+        Male
+      </label>
+      <label>
+        <input type="radio" name="gender" value= "female" checked = {gender == "female"} onChange={handleGenderChange} />
+        Female
+      </label>
+
+      {
+        gender ==="male" && (
+          <div>
+            <label htmlFor="">Select your shirt size :</label>
+            <br />  
+            <select name="" id="" value={size} onChange={handleSizeChange}>
+              <option value="">Select</option>
+              <option value="Small">Small</option>
+              <option value="medium">medium</option>
+              <option value="large">large</option>
+            </select>
+          </div>
+        )
+      }
+      {
+        gender === "female" && (
+          <div>
+            <label htmlFor="">Select your dress size : </label>
+            <br />
+            <select name="" id="" value={size} onChange={handleSizeChange}>
+            <option value="">Select</option>
+            <option value="2">2</option>
+            <option value="4">4</option>
+            <option value="6">6</option>
+            </select>
+          </div>
+        )
+      }
+
     </div>
   )
 }
